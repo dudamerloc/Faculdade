7877505f2aeb0a8c6164e8ec59b1a7d0bc0b1810
MARIA EDUARDA MERLO CANDIDO <maria.candido@cs.up.edu.br>
2025-05-21 09:07:42 -0300

    PHP

--- aula04/form_pesquisa.php
+++
@@ -1,7 +0,0 @@
-<form action=" resultado.php" method="get">
-
-<p>
-    <input type="text" name="cliente" placeholder="Nome do Cliente"></input>
-    <button type="submit">Pesquisar</button>
-</p>
-</form>
